4cca901c2cf3b5c0f35abd409c8701fde6a389fc
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-11-02 10:34:20 +0100

    modifiche scroll

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -5,15 +5,18 @@ import Search from "../components/Search"
 import navListData from "../data/navListData"
 import "./header.css"
 
-const Header = ({ scroll, nav }) => {
+const Header = ({ scroll }) => {
   const [navList, setNavList] = useState(navListData)
 
   const handelNavOnClick = (id) => {
-    setNavList((nav.active = false))
-    if (nav._id === id) {
-      nav.active = true
+    const newList = navList.map((nav) => {
+      nav.active = false
+      if (nav._id === id) {
+        nav.active = true
+      }
       return nav
-    }
+    })
+    setNavList(newList)
   }
 
   return (
